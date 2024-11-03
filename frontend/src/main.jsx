f48caf881abae9313c159c3178263d5840ee0ac8
Andre Silva <andrenations@gmail.com>
2024-11-02 21:34:11 -0400

    initial commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import './index.css'
+import App from './App.jsx'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+
+
+const  router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      {/* <Route path="/cart" element={<CartScreen />} /> */}
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />  
+  </StrictMode>,
+)
