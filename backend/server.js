13646a31b48beef1fc63ff8e4b4f713435ec391c
Andre Silva <andrenations@gmail.com>
2024-11-03 20:20:06 -0500

    feat: establish MongoDB connection with connectDB
    - Imported connectDB from "./config/db.js".
    - Called connectDB() to initiate a connection to the MongoDB database.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config(); 
 import products from "./data/products.js";
+
+import connectDB from "./config/db.js";
+
 const port = process.env.PORT || 4000;
+
+connectDB();
 
 const app = express();
 
