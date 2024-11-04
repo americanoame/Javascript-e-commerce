2790fb9a5ed64370eaf41bda98cd6815cd0cc860
Andre Silva <andrenations@gmail.com>
2024-11-03 19:52:17 -0500

    add  product data

---
+++ backend/data/products.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Zenith Over-Ear Wireless Headphones',
+    image: '/images/01.png',
+    hoverImage: '/images/02.png',
+    description:
+      'Elevate your listening experience with the Zenith Over-Ear Wireless Headphones. Featuring premium sound quality, active noise cancellation, and up to 40 hours of playtime, these headphones are perfect for immersive, all-day listening. The lightweight, ergonomic design ensures comfort, making them ideal for travel, work, or relaxation.',
+    category: 'Headphones',
+    price: 199.99,
+    countInStock: 12,
+    rating: 4.7,
+    numReviews: 18,
+    additionalImages: ['/images/01.png', '/images/01.png', '/images/01.png', '/images/01.png'],
+  },
+  {
+    _id: '2',
+    name: 'Echo ANC Wireless Earbuds',
+    image: '/images/03.png',
+    hoverImage: '/images/04.png',
+    description:
+      'Designed for on-the-go convenience, Echo ANC Wireless Earbuds provide a rich audio experience with active noise cancellation and water-resistant features. These earbuds offer up to 24 hours of playtime with the charging case, and their compact design makes them perfect for workouts, commuting, or casual listening.',
+    category: 'Headphones',
+    price: 129.99,
+    countInStock: 5,
+    rating: 4.3,
+    numReviews: 25,
+    additionalImages: ['/images/03.png', '/images/03.png', '/images/03.png', '/images/03.png'],
+  },
+  {
+    _id: '3',
+    name: 'Pulse Pro Wired Studio Headphones',
+    image: '/images/05.png',
+    hoverImage: '/images/06.png',
+    description:
+      'Built for professionals, Pulse Pro Wired Studio Headphones deliver clear, accurate sound, ideal for music production and critical listening. These headphones feature a durable design and superior comfort, allowing for extended wear during recording sessions or mixing. With enhanced bass response and balanced audio, they meet the demands of audiophiles and professionals alike.',
+    category: 'Headphones',
+    price: 159.99,
+    countInStock: 7,
+    rating: 4.6,
+    numReviews: 10,
+    additionalImages: ['/images/05.png', '/images/05.png', '/images/05.png', '/images/05.png'],
+  },
+  {
+    _id: '4',
+    name: 'Wave Lite On-Ear Bluetooth Headphones',
+    image: '/images/07.png',
+    hoverImage: '/images/07.png',
+    description:
+      'Wave Lite On-Ear Bluetooth Headphones provide a balance of portability and high-quality sound. With a foldable design and 30 hours of battery life, these headphones are excellent for casual listening and easy to carry. Enhanced with deep bass and clear vocals, they suit music lovers on a budget.',
+    category: 'Headphones',
+    price: 79.99,
+    countInStock: 0,
+    rating: 4.1,
+    numReviews: 8,
+    additionalImages: ['/images/07.png', '/images/07.png', '/images/07.png', '/images/07.png'],
+  },
+
+  {
+    _id: '5',
+    name: 'Wave Lite Analog Watch',
+    image: '/images/12.png',
+    hoverImage: '/images/13.png',
+    description:
+      'The Wave Lite Analog Watch combines elegance with durability. Featuring a sleek design and a water-resistant casing, it’s perfect for both everyday wear and special occasions. With a long-lasting battery and precision quartz movement, this watch offers both style and reliability for those on the go.',
+    category: 'Watches',
+    price: 99.99,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 12,
+    additionalImages: ['/images/12.png', '/images/12.png', '/images/12.png', '/images/12.png'],
+  },
+];
+
+export default products;
