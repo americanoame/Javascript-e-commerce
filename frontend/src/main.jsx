2f9eb235addfc7f2bab59230a083209651763401
Andre Silva <andrenations@gmail.com>
2024-11-07 16:52:18 -0500

    Integrate Redux Provider in the main entry point
    - Wrapped the app with the Redux `Provider` to connect the Redux store to the React application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import './index.css'
-import App from './App.jsx'
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Provider } from "react-redux";
+import store from "./store";
 
-
-
-const  router = createBrowserRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       {/* <Route path="/cart" element={<CartScreen />} /> */}
     </Route>
   )
-)
+);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />  
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
