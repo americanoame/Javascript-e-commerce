5a2ba9366d227b4f12d3177ffeaadee2fd3b1d59
Andre Silva <andrenations@gmail.com>
2024-11-09 22:02:39 -0500

    Added pre-save hook to hash password before saving
    - Implemented `userSchema.pre('save')` middleware to hash the user's password before storing it in the database.
    - Added a check to prevent re-hashing the password if it hasn't been modified, improving efficiency.
    - Integrated bcrypt to securely hash passwords with a salt factor of 10.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -24,6 +24,7 @@ const userSchema = mongoose.Schema({
     timestamps: true
 })
 
+// since we add metthod to the schema no we can access this method on any user object (user.matchPassword())
 userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
@@ -33,22 +34,21 @@ userSchema.methods.matchPassword = async function(enteredPassword) {
 // .pre() is a middleware function that runs before a specific event
 // In this case, we want to hash the password before saving the user
 
-
-// userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function(next) {
     
     // if the password is not modified we don't want to hash it again
-    // if (!this.isModified('password')) {
-    //     next()
-    // }
+    if (!this.isModified('password')) {
+        next()
+    }
 
     
-//     const salt = await bcrypt.genSalt(10) 
-//     this.password = await bcrypt.hash(this.password, salt)
+    const salt = await bcrypt.genSalt(10) 
+    this.password = await bcrypt.hash(this.password, salt)
     
-// })
+})
 
 // once those user is registered we i wanna authticate the user besically
-// we iwanna create out json web token and we iwanna create our http only cookie
+// we iwanna create  json web token and we iwanna create our http only cookie
 // but lets create a separate file for that in the utils folder called generateToken.js 
 
 const User = mongoose.model('User', userSchema)
