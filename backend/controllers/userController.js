ebc389401d95aadb82a5b44f80e9b9d1768b717f
Andre Silva <andrenations@gmail.com>
2024-11-09 21:55:28 -0500

    Implemented user authentication and profile management endpoints
    - Added authentication logic in authUser to validate user credentials and generate a token.
    - Implemented user registration in registerUser, including validation for existing users.
    - Created logoutUser to clear the JWT token cookie and log the user out.
    - Added getUserProfile and updateUserProfile to allow users to view and update their profiles.
    - Added getUsers, getUserByID, deleteUser, and updateUser endpoints for admin access to manage users.
    - Ensured password hashing is handled in the user model for clean code.
    - Used asyncHandler for handling asynchronous operations and error management.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,28 +1,84 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Auth user & get token
 // @route   GET /api/users/login
 // @access  Public
-const authUser = asyncHandler(async (req, res) => { 
-  res.send("auth user");
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    // we are calling the function generateToken from the utils folder
+    // and passing the response object and the user id to it
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      // token: generateToken(user._id),
+      // we could send it back in the json and we srtore in the frontend
+      // in local storage but we will use cookies instead of local storage,
+      // so we send the token in the cookie on the server and we can
+      // access it on the client side for security reasons
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 
 
 
+
+// then we have to hash the password before saving it to the database in oder to compare it with the password that the user entered
+// we can do this here but in order to keep the code clean we will do it in the user model
+
 // @desc    Register user
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  
-  res.send("register user");
+  const { name, email, password } = req.body;
+
+  // Check if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // if the user doesn't exist we i wanna Create a new user
+  const user = await User.create({
+    name,
+    email,
+    password,
   });
 
-  
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 
-// then we have to hash the password before saving it to the database
-// we can do this here but in order to keep the code clean we will do it in the user model
+  // then we have to hash the password before saving it to the database
+  // we can do this here but in order to keep the code clean we will do it in the user model
+});
+
+
+
+
 
 
 
@@ -30,10 +86,16 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route    Post /api/users/logout
 // @access   Private
 const logoutUser = asyncHandler(async (req, res) => {
-  
-
-  res.send("Logged out successfully");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).send("Logged out successfully");
 });
+
+
+
+
 
 
 
@@ -42,22 +104,61 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @access   Private
 const getUserProfile = asyncHandler(async (req, res) => {
   // when we're login we have access to the user object in the request object so we can pass (req.user._id);
-  
-  res.send("get user profile");
+
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
-
-
-
 
 // @desc    Update user profile
 // @route    PUT /api/users/profile
 // @access   Private
 
+
+
 const updateUserProfile = asyncHandler(async (req, res) => {
-  
-  
-  res.send("update user profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    console.log(req.body);
+    // we only wanna update field that we send in the request body 
+    // in this case we only wanna update the name and email || if it's not there we wanna keep the old value
+    // 
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email; // the reason we are doing this is beacuse the password is hashed 
+    // and we don't wanna update it unless the user send the password  if (req.body.password) {
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    
+    // then we going to save the user then will return the user data and we will put on updataedUser variable 
+    const updatedUser = await user.save();
+
+    // then we will send the updated user data back to the client
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
+
+
 
 
 
@@ -66,8 +167,24 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @access   Private/Admin
 
 const getUsers = asyncHandler(async (req, res) => {
-  res.send("get  users");
+  // when we're login we have access to the user object in the request object so we can pass (req.user._id);
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
+
+
+
 
 
 // @desc     Get user by ID
@@ -81,6 +198,7 @@ const getUserByID = asyncHandler(async (req, res) => {
 
 
 
+
 // @desc     Delete user
 // @route    DELETE /api/users/:id
 // @access   Private/Admin
@@ -89,8 +207,6 @@ const deleteUser = asyncHandler(async (req, res) => {
   res.send("delete user");
 });
 
-
-
 // @desc     Update user
 // @route    PUT /api/users/:id
 // @access   Private/Admin
@@ -98,7 +214,6 @@ const deleteUser = asyncHandler(async (req, res) => {
 const updateUser = asyncHandler(async (req, res) => {
   res.send("update user");
 });
-
 
 export {
   authUser,
