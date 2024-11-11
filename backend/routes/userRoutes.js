ac47dd66677985c7069dc5aa2f9ee6e59d6aeabb
Andre Silva <andrenations@gmail.com>
2024-11-11 14:31:42 -0500

    fix the routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,7 +13,6 @@ import {
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-
 router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/logout", logoutUser);
 router.post("/auth", authUser);
@@ -21,6 +20,10 @@ router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
-  router.route("/:id").delete(protect, admin, deleteUser).get(protect, admin, getUserByID).put(protect, admin, updateUser);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserByID)
+  .put(protect, admin, updateUser);
 
 export default router;
