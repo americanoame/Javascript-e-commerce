9dfcd434b7827609d7f11a0c0418fa04bf9fdf3a
Andre Silva <andrenations@gmail.com>
2024-11-11 14:27:31 -0500

    Added /api/orders route for order management and structured API handling

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 const port = process.env.PORT || 4000;
 
@@ -29,6 +30,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 
 app.use(notFound);
