90576434b7546b64c777870bcdf42be3a73165d2
Andre Silva <andrenations@gmail.com>
2024-11-12 18:20:26 -0500

    Refactor Footer component with updated width and background color
    - Increased footer width to 2230px and centered it using max-width
    - Applied a new background color (bg-orange-200) for improved styling
    - Added shadow effect (shadow-2xl) for a more distinct footer appearance
    - Updated footer text styling with bold and white color for better visibility

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full max-w-[2200px]  mx-auto  shadow-2xl bg-gray-100  h-[70px] ">
+    <footer className="w-full max-w-[2230px]  mx-auto  shadow-2xl  bg-orange-200  h-[70px] ">
       <div className="w-full h-full flex items-center justify-center">
         <div className="">
-          <span className="  text-gray-950 ">
+          <span className="text-white  font-bold">
             <p>HeadPopp &copy; {currentYear}</p>
           </span>
         </div>
