0c75ed204a449228d310c30e08c8990c47caba32
Andre Silva <andrenations@gmail.com>
2024-11-12 18:25:46 -0500

    Refactor Product component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -25,27 +25,26 @@ const Product = ({ product }) => {
         </figure>
 
         <div className="absolute inset-0 transform translate-y-full group-hover:translate-y-0 duration-500 ease-in-out  items-center justify-center px-1 rounded-full opacity-0 group-hover:opacity-100 w-12 h-24 lg:flex hidden">
-          <button className="text-white text-[12px] font-thin bg-pink-950 bg-opacity-90 py-2 px-1 rounded-full shadow-2xl">
+          <button className="text-white text-[12px] font-thin bg-red-500  bg-opacity-90 py-2 px-1 rounded-full shadow-2xl gradient-text">
             ADD TO CART
           </button>
         </div>
       </Link>
 
       {/* Product name */}
-      <div className="mt-4 w-full text-center px-7 text-gray-900">
+      <div className="mt-4 w-full text-center px-7 text-white">
         <h2 className="text-2xl tracking-widest font-extralight sm:text-xl">
           {product.name}
         </h2>
       </div>
 
       {/* Product price */}
-      <div className="mt-2 w-full font-thin text-center px-7 text-gray-900 sm:mt-1">
+      <div className="mt-2 w-full font-thin text-center px-7 text-gray-600 sm:mt-1">
         <span className="text-lg font-normal sm:text-base">
           ${product.price}
         </span>
       </div>
 
-      
       {/* Product rating */}
       <div className="mt-2 w-full text-center px-7 text-gray-900">
         <Rating value={product.rating} text={`${product.numReviews} reviews`} />
