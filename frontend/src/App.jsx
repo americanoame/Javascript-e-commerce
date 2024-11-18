c97713a4d552f988d6fdf0100f411ec7f17bba5e
Andre Silva <andrenations@gmail.com>
2024-11-18 16:10:28 -0500

    Implement ScrollToTop functionality to ensure the page always scrolls to the top on route change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 
 const App = () => {
   return (
     <>
+     <ScrollToTop />
       <Header />
-
       <div id="app-root" className="flex flex-col min-h-screen">
         <main>
           <Outlet />
