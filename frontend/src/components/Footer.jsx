e43ba595a3ff62de7e6e3858206e8091bf2f967f
Andre Silva <andrenations@gmail.com>
2024-11-18 00:00:53 -0500

    refactor: Enhance Footer component with gradient background and additional links
    - Updated the footer's styling to use a linear gradient background.
    - Improved structure with a responsive layout for better user experience.
    - Added "About," "Contact," and "Privacy Policy" links with hover effects.
    - Optimized class usage for alignment and spacing.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,12 +4,35 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full max-w-[2230px]  mx-auto  shadow-2xl  bg-orange-200  h-[70px] ">
-      <div className="w-full h-full flex items-center justify-center">
-        <div className="">
-          <span className="text-white  font-bold">
-            <p>HeadPopp &copy; {currentYear}</p>
-          </span>
+    <footer className="w-full max-w-[2230px]  mx-auto  shadow-2xl h-[70px]"
+    style={{
+      background: "linear-gradient(to right, #feb47b, #ff7e5f)",
+    }}
+    >
+      <div className="w-full text-center max-w-[1200px] mx-auto md:px-8 md:pt-5 px-4 pt-4">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          
+          <p className="pb-1 font-bold">HeadPopp &copy; {currentYear}</p>
+          
+          <ul className="flex flex-wrap justify-center items-center mb-6 text-sm font-bold  sm:mb-0 dark:text-gray-950">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                About
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Contact
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="hover:underline ">
+                Privacy Policy
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
