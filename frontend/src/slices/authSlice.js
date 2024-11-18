c70594a368628ac99f89180a4082368e796e5c13
Andre Silva <andrenations@gmail.com>
2024-11-18 00:17:13 -0500

    Add auth slice for user login and logout management
    - Implemented `authSlice` using Redux Toolkit to manage user authentication state.
    - On login, user credentials are saved in Redux state and localStorage using `setCredentials` action.
    - On logout, user information is removed from both Redux state and localStorage.
    - Added logic to automatically retrieve user info from localStorage during app initialization.
    - Ensured that the user's credentials are available across sessions.

---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,34 @@
+// Desc: Auth slice to handle user login and logout
+
+import { createSlice } from "@reduxjs/toolkit";
+
+// localStorage get usserInfo from local storage if it exists, else set it to null
+// its gonna be stored in local storage as a string, so we need to parse it back to javascript object
+
+const initialState = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+// then we need to sent the useIinfo to the payload (action.payload)
+    setCredentials: (state, action) => { // setCredentials right after registration also logs the user in immediately by storing their credentials in the state, enabling instant access without requiring a separate login step.
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+      // and the user gets put in the local storage user name and password 
+    },
+    logout: (state, action) => {
+      state.userInfo = null;
+      localStorage.removeItem("userInfo"); 
+    },
+  },
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+
+
+export default authSlice.reducer;
