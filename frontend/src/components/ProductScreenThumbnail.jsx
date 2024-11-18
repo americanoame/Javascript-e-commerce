a0c52e316519dcd724d5c4c39deb302c6457f3dd
Andre Silva <andrenations@gmail.com>
2024-11-18 00:02:25 -0500

    refactor: Improve ProductScreenThumbnail component with enhanced styling and navigation
    - Added a "GO BACK" link for improved navigation, styled with emphasis.
    - Enhanced layout with responsive spacing and rounded container design.
    - Optimized additional image thumbnails with flexible dimensions for better responsiveness.
    - Centralized thumbnail display for a more cohesive UI.

--- frontend/src/components/ProductScreenThumbnail.jsx
+++ frontend/src/components/ProductScreenThumbnail.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ProductScreenThumbnail = ({ mainImage, additionalImages, onSelectImage }) => {
+const ProductScreenThumbnail = ({
+  mainImage,
+  additionalImages,
+  onSelectImage,
+}) => {
   return (
-    <div className="flex-shrink-0 w-full  md:w-3/5 lg:w-2/5 bg-gray-100 p-7">
-      <img
-        src={mainImage}
-        alt="Product"
-        className="object-cover w-full h-auto md:max-w-full"
-      />
-      {additionalImages && (
-        <div className="flex mt-4 space-x-3">
-          {/* Show additional images, making sure not to repeat the main image */}
-          {additionalImages.map((img, index) => (
-            <img
-              key={index}
-              src={img}
-              alt={`Thumbnail ${index + 1}`}
-              onClick={() => onSelectImage(img)}
-              className="w-16 h-16 object-cover border p-2 border-gray-300 cursor-pointer hover:border-red-500"
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  )
-}
+    <div className="space-y-1 sm:ml-4 md:ml-6 lg:ml-8 xl:ml-10">
+      <Link to="/" className="text-start">
+        <div className=" text-red-800 ">⇠ GO BACK</div>
+      </Link>
 
-export default ProductScreenThumbnail
+      <div className="bg-gray-900 p-5 rounded-lg mx-4 sm:mx-6 md:mx-auto">
+        <img
+          src={mainImage}
+          alt="Product"
+          className="object-cover h-auto md:max-w-full mr-2"
+        />
+        {additionalImages && (
+          <div className="flex mt-4 space-x-3 justify-center text-center">
+            {additionalImages.map((img, index) => (
+              <img
+                key={index}
+                src={img}
+                alt={`Thumbnail ${index + 1}`}
+                onClick={() => onSelectImage(img)}
+                className="w-14 h-16 sm:w-6 sm:h-6 md:w-20 md:h-20 object-cover border p-2 border-gray-300 cursor-pointer hover:border-red-500"
+                
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductScreenThumbnail;
