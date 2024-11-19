b32efd1cf7610f7452e25d41f285ef2ce5bcab65
Andre Silva <andrenations@gmail.com>
2024-11-19 14:23:52 -0500

    Extend Tailwind CSS breakpoints for iPad Pro, Nest Hub, and Nest Hub Max

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        ipadpro: { raw: "(min-width: 1024px) and (max-width: 1366px)" },
+        nesthub: { raw: "(max-width: 1024px) and (max-height: 600px)" },
+        nesthubmax: { raw: "(max-width: 1280px) and (max-height: 800px)" },
+      },
+    },
   },
   plugins: [],
 }
