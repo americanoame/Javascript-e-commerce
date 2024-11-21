ee41c1c3b5d9b0d9c8ba100a6c21a314cf5159d8
Andre Silva <andrenations@gmail.com>
2024-11-21 15:44:22 -0500

    - Added <Toaster /> from react-hot-toast to handle notifications globally.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
+import { Toaster } from "react-hot-toast";
 
 
 const App = () => {
   return (
     <>
+    <Toaster  />
      <ScrollToTop />
       <Header />
       <div id="app-root" className="flex flex-col min-h-screen">
